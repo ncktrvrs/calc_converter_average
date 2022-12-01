bfd4192e01533c46d47ae3c1636ed83bd430534a
U <ncktrvrs@gmail.com>
2022-12-01 19:16:36 +0200

    the first program was added

---
+++ Calculator/script.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+  result = a + b;
+  alert(`The result of summing is: ${result}`);
+}
+
+let a = +prompt("Enter the first number");
+let b = +prompt("Enter the second number");
+
+sum(a, b);
